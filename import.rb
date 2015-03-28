c51b78a2d1af9152e3a2d4e2d6b756e4de96a211
Ben Halverson <benhalverson33@gmail.com>
2015-03-27 19:54:05 -0700

    added more categories

--- import.rb
+++ import.rb
@@ -1,9 +1,9 @@
 require 'CSV'
 
-filename = 'input.txt'
+filename = 'moredata.txt'
 
 count = 0
-CSV.open('output.csv', 'wb') do |csv|
+CSV.open('moredata-output.csv', 'wb') do |csv|
 	File.open(filename, 'r').each_line do |line|
 	if line != ''
 		line = line.encode('UTF-8', :invalid => :replace)
@@ -12,13 +12,13 @@ CSV.open('output.csv', 'wb') do |csv|
 row = row.slice(0..-2)
 
 #indexes to use are 0, 1, 11, 4, 6, 17
-ordered_row =[row[0], row[1], row[11], row[4], row[6], row[17]]
+ordered_row =[row[0], row[1], row[11], row[23], row[9], row[10], row[21], row[22], row[4], row[6], row[17]]
 
 csv << ordered_row
 
 puts count
 #comment out break to run the complete file.
-break if count == 5
+#break if count == 5
 count = count +1
 row
 	end
