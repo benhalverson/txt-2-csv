e7635219fbfb03f34ff1837906384c4aa6acbc36
Ben Halverson <benhalverson33@gmail.com>
2015-05-18 14:36:58 -0700

    update gitignore

--- import.rb
+++ import.rb
@@ -1,9 +1,9 @@
 require 'CSV'
 
-filename = 'moredata.txt'
+filename = 'business/25952.txt'
 
 count = 0
-CSV.open('moredata-output.csv', 'wb') do |csv|
+CSV.open('business/csv/25952-output.csv', 'wb') do |csv|
 	File.open(filename, 'r').each_line do |line|
 	if line != ''
 		line = line.encode('UTF-8', :invalid => :replace)
@@ -11,9 +11,8 @@ CSV.open('moredata-output.csv', 'wb') do |csv|
 		row = line.split('|')
 row = row.slice(0..-2)
 
-#indexes to use are 0, 1, 11, 4, 6, 17
-ordered_row =[row[0], row[1], row[11], row[23], row[9], row[10], row[21], row[22], row[4], row[6], row[17]]
-
+# ordered_row =[row[0], row[1], row[11], row[23], row[9], row[10], row[21], row[22], row[4], row[6], row[17]]
+ordered_row =[row[1],row[3],row[4],row[5],row[9], row[10], row[11], row[21], row[23]]
 csv << ordered_row
 
 puts count
